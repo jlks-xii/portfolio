22dca99567a015dd5fba83e78df31ad9ebfd0d43
Jeremy <jlks94@gmail.com>
2021-05-31 12:16:17 +0800

    deploy

---
+++ precache-manifest.bdc88fabf4c62daf2f5d474f810e2adf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8ee2775bc7f913c8387",
+    "url": "/portfolio/css/about.8f4911a8.css"
+  },
+  {
+    "revision": "c8ee2775bc7f913c8387",
+    "url": "/portfolio/js/about.fa1ee653.js"
+  },
+  {
+    "revision": "0a3bed4a2a741a7646ab",
+    "url": "/portfolio/css/app.96c963de.css"
+  },
+  {
+    "revision": "0a3bed4a2a741a7646ab",
+    "url": "/portfolio/js/app.ee7045a5.js"
+  },
+  {
+    "revision": "ba42b73ce07ad5731918",
+    "url": "/portfolio/js/chunk-vendors.f350d9f2.js"
+  },
+  {
+    "revision": "e663748100841006a7ab9da9d3e49a9a",
+    "url": "/portfolio/fonts/Cardo-Regular.e6637481.ttf"
+  },
+  {
+    "revision": "16ad84213cd06ec9ca2986617e474c6c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  }
+];
